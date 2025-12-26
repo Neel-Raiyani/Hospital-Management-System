cdf38bd68469b20b53bac742d556dde6b4aa9829
Neel-Raiyani <neelr.empyreal@gmail.com>
2025-12-26 18:49:21 +0530

    Added validation using express-validator

--- Services/Auth-service/src/routes/authRoutes.ts
+++ Services/Auth-service/src/routes/authRoutes.ts
@@ -1,11 +1,19 @@
 import express from "express";
+import { body } from "express-validator"
 import { login, createUser, changePassword } from "@controllers/authController.js"
 import { auth } from "@middlewares/auth.js";
 import { role } from "@middlewares/role.js";
+import validate from "@middlewares/validation.js";
 
 const router = express.Router();
 
-router.post('/create', auth, role("ADMIN"), createUser);
+const createUserValidation = [
+    body("name").notEmpty().withMessage("Name is Required"),
+    body("email").isEmail().withMessage("Valid email is required"),
+    body("role").notEmpty().withMessage("Role is required").isIn(["ADMIN", "DOCTOR", "LAB", "RECEPTIONIST", "PATIENT"]).withMessage("Invalid Role!!!")
+]
+
+router.post('/create', auth, role("ADMIN"), createUserValidation, validate, createUser);
 router.post('/login', login);
 
 router.patch('/change-password', auth, changePassword);
