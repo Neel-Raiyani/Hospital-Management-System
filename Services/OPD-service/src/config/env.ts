077106782e5f349f3f2d5de59a479f7e2800febb
Neel-Raiyani <neelr.empyreal@gmail.com>
2026-01-05 13:12:47 +0530

    chore(OPD): Initialize TypeScript, configure Prisma ORM, integrate ESLint + Prettier, and setup MongoDB connection

---
+++ Services/OPD-service/src/config/env.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const env = {
+    port: process.env.PORT || 5019,
+    mongo_URI: process.env.DATABASE_URL as string,
+    jwt_secret: process.env.JWT_SECRET as string,
+};
+
+export default env;
