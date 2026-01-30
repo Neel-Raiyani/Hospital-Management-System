bef40a87690e7c7ec845851e67e7167e8854bca6
Neel-Raiyani <neelr.empyreal@gmail.com>
2026-01-30 19:30:16 +0530

    feat(patient): Expand access to ADMIN and LAB roles

--- Services/Patient-service/src/routes/patientRoutes.ts
+++ Services/Patient-service/src/routes/patientRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {createValidation, updateValidation, validate} from "@middlewares/validation.js";
+import { createValidation, updateValidation, validate } from "@middlewares/validation.js";
 import auth from "@middlewares/auth.js";
 import role from "@middlewares/role.js";
 import { createPatient, deactivatePatient, getPatientById, listPatients, updatePatient } from "@controllers/patientController.js";
@@ -7,8 +7,8 @@ const router = express.Router();
 
 router.post('/create', auth, role("RECEPTIONIST"), createValidation, validate, createPatient);
 
-router.get('/list', auth, role("RECEPTIONIST", "DOCTOR"), listPatients);
-router.get('/:id', auth, role("RECEPTIONIST", "DOCTOR"), getPatientById);
+router.get('/list', auth, role("RECEPTIONIST", "DOCTOR", "ADMIN", "LAB"), listPatients);
+router.get('/:id', auth, role("RECEPTIONIST", "DOCTOR", "ADMIN", "LAB"), getPatientById);
 
 router.patch('/update/:id', auth, role("RECEPTIONIST"), updateValidation, validate, updatePatient);
 router.patch('/deactivate/:id', auth, role("ADMIN"), deactivatePatient)
